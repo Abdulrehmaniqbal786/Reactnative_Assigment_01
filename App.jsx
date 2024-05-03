4e59f3883945b9072e49578e4aba02595e5dd570
Abdulrehman Iqbal <abdullahiqbal83020@gmail.com>
2024-05-03 15:33:55 -0700

    compelete

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>"JAWAN PAKISTAN TECH"</Text>
+      <Text> AbdulRehman </Text>
+      <Text> Muhammad Iqbal </Text>
+      <Text> Class : Xl </Text>
+      <Text> I Am A Complete Web And Mobile App Developer!  </Text>
+    </View>
+  )
+}
+
+export default App
